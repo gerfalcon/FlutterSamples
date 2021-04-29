d3436543a91b424356c1b3e2d9da44779acf1d69
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-04-29 22:00:14 +0200

    3. error MyApp can't be StatefulWidget

--- main.dart
+++ main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
