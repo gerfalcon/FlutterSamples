5d48cd6e041596b8a4eeaa0335118ca74f76bed9
Pásztor Dániel <danim1130@users.noreply.github.com>
2021-05-02 17:58:09 +0200

    Removed TODO version

--- main_big_debugging_task.dart
+++ main_big_debugging_task.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() <= runApp(MyApp()); //TODO: Bad arrow syntax.
+void main() <= runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,18 +11,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget { //TODO: StatefulWidget.
-  final String title; //TODO: Missing constructor.
+class MyHomePage extends StatelessWidget {
+  final String title;
   
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final int _counter = 0; //TODO: _counter can't be final.
+  final int _counter = 0;
   final repository = Repository();
 
-  void incrementCounter_() { //TODO: It's not an error :D.
+  void incrementCounter_() {
     setState(() {
       _counter++;
     });
@@ -30,13 +30,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    repository.getDataFormServerAsync(); //TODO: don't call long methods at the top of build(). TODO: Can't wait their async result here.
+    repository.getDataFormServerAsync();
     return Scaffold(
-      body: buildBodyWidget(context), //TODO: Don't use buildWidget() functions.
+      body: buildBodyWidget(context),
       floatingActionButton: FloatingActionButton(
-        onPressed: [ //TODO: Wrong syntacs of onPressed.
+        onPressed: [
           incrementCounter_();
-          Navigator.of().pop(); //TODO: missing context.
+          Navigator.of().pop();
         ],
       ),
     );
@@ -44,12 +44,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildBodyWidget(BuildContext context) {
     return Container(
-      children: [ //TODO: Container has only one child.
+      children: [
         Text('You have pushed the button this many times:'),
-        SizedBox(width: 5000), //TODO: Unrealistic size.
+        SizedBox(width: 5000),
         GestureDetector(
           child: Text('$_counter'),
-          onDoubleTap: Scaffold.of(context).showSnackBar(SnackBar()), //TODO:bad syntax of DoubleTap //TODO: Scaffold.of() called with a context that does not contain a Scaffold.
+          onDoubleTap: Scaffold.of(context).showSnackBar(SnackBar()),
         )
       ],
     );
